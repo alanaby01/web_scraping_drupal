e306d0a8f6207dd22910768cb567a8b49f40223a
Alan P Aby <alanaby01@gmail.com>
2022-09-16 15:05:53 -0400

    Finished

--- drupal.py
+++ drupal.py
@@ -2,28 +2,32 @@
 from unicodedata import name
 from bs4 import BeautifulSoup
 import urllib.request
-quote_page = 'https://www.drupal.org/project/project_module?f%5B0%5D=&f%5B1%5D=&f%5B2%5D=&f%5B3%5D=&f%5B4%5D=sm_field_project_type%3Afull&f%5B5%5D=&f%5B6%5D=&text=&solrsort=iss_project_release_usage+desc&op=Search'
+project_url_list = []
+profile_url_list = []
+quote_page = 'https://www.drupal.org/project/project_module?f%5B0%5D=&f%5B1%5D=&f%5B2%5D=&f%5B3%5D=&f%5B4%5D=sm_field_project_type%3Afull&f%5B5%5D=&f%5B6%5D=&text=&solrsort=iss_project_release_usage%20desc&op=Search'
 weburl = urllib.request.urlopen(quote_page)
 soup = BeautifulSoup(weburl, 'html.parser')
-name_box = soup.find('div', attrs={'class': 'node'}).find('h2').find('a').get('href')
-print(name_box)
+project_box = soup.findAll('div', attrs={'class': 'node'})
+for name in project_box:
+    project_url_list.append("https://www.drupal.org"+ name.find('h2').find('a').get('href'))
 #project page
 
-project_url = "https://www.drupal.org"+ name_box
-print(project_url)
-projecturl = urllib.request.urlopen(project_url)
-soup2 = BeautifulSoup(projecturl, 'html.parser')
-name_box  = soup2.find('div',attrs={'class':'user-picture'}).find('a').get('href')
-print(name_box)
+for project_url in project_url_list:
+    projecturl = urllib.request.urlopen(project_url)
+    soup2 = BeautifulSoup(projecturl, 'html.parser')
+    profile_box  = soup2.findAll('div',attrs={'class':'user-picture'})
+    for profile in profile_box:
+        profile_url_list.append("https://www.drupal.org" + profile.find('a').get('href'))
+# profile page
 
-#profile page
-
-profile_url = "https://www.drupal.org" + name_box
-print(profile_url)
-profileurl = urllib.request.urlopen(profile_url)
-soup3 = BeautifulSoup(profileurl, 'html.parser')
-name_box  = soup3.find('h1',attrs={'id':'page-title'}).text
-print(name_box)
-social_box = soup3.find('div',attrs={'class':'field-name-field-social-links'}).findAll('div', attrs={'class':'field-item'})
-for social in social_box:
-    print(social.find('a').get('href'))
+for profile_url in profile_url_list:
+    profileurl = urllib.request.urlopen(profile_url)
+    soup3 = BeautifulSoup(profileurl, 'html.parser')
+    name_box  = soup3.find('h1',attrs={'id':'page-title'}).text
+    print(name_box)
+    try:
+        social_box = soup3.find('div',attrs={'class':'field-name-field-social-links'}).findAll('div', attrs={'class':'field-item'})
+        for social in social_box:
+            print(social.find('a').get('href'))
+    except:
+        print("no social media")
